31230e16727eb0ebef5077e3601a0f88e049c217
Daniel de Schiffart <danieldeschiffart@gmail.com>
2019-02-18 01:29:55 -0100

    Updated debug routines

--- main.lua
+++ main.lua
@@ -7,6 +7,8 @@ local data = {
 	heading = 0,
 }
 
+local pitchmax = 90
+
 local keymap = {
 	escape = function() love.event.quit() end,
 }
@@ -19,15 +21,27 @@ end
 
 -- Main LOVE functions
 function love.load()
+	local pitch_up = true
 end
 
 function love.update(dt)
+	--[[
 	data.altitude = data.altitude + (math.random() - 0.5) * 10
 	data.ias = data.ias + (math.random() - 0.5) * 10
 	data.vspeed = data.vspeed + (math.random() - 0.5) * 10
 	data.pitch = data.pitch + (math.random() - 0.5) * 5
 	data.roll = data.roll + (math.random() - 0.5) * 10
 	data.heading = data.heading + (math.random() - 0.5) * 10
+	--]]
+	if data.pitch > pitchmax then
+		data.pitch = pitchmax
+		pitch_up = false
+	end
+	if data.pitch < -pitchmax then
+		data.pitch = -pitchmax
+		pitch_up = true
+	end
+	if pitch_up then data.pitch = data.pitch + 1 else data.pitch = data.pitch - 1 end
 end
 
 function love.draw()
