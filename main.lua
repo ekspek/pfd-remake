f4588edb224b30bedc2b6a2645cbb11e97eaaf39
Daniel de Schiffart <danieldeschiffart@gmail.com>
2019-02-19 15:29:26 -0100

    Added B612 fonts, pitch labels

--- main.lua
+++ main.lua
@@ -22,6 +22,10 @@ end
 
 -- Main LOVE functions
 function love.load()
+	sans = love.graphics.newFont("fonts/B612-Regular.ttf", 15)
+	mono = love.graphics.newFont("fonts/B612Mono-Regular.ttf", 15)
+	sans_label = love.graphics.newFont("fonts/B612-Regular.ttf", 15)
+
 	local pitch_up = true
 	local roll_left = true
 end
@@ -43,7 +47,7 @@ function love.update(dt)
 		data.pitch = -pitchmax
 		pitch_up = true
 	end
-	if pitch_up then data.pitch = data.pitch + 1 else data.pitch = data.pitch - 1 end
+	if pitch_up then data.pitch = data.pitch + 0.3 else data.pitch = data.pitch - 0.3 end
 	if data.roll > rollmax then
 		data.roll = rollmax
 		roll_left = false
@@ -52,7 +56,8 @@ function love.update(dt)
 		data.roll = -rollmax
 		roll_left = true
 	end
-	if roll_left then data.roll = data.roll + math.pi / 180 else data.roll = data.roll - math.pi / 180 end
+	--if roll_left then data.roll = data.roll + math.pi / 180 else data.roll = data.roll - math.pi / 180 end
+	data.roll = 10 * math.pi / 180
 end
 
 function love.draw()
@@ -102,19 +107,22 @@ function love.draw()
 	love.graphics.polygon('fill', horizon)
 
 	-- Pitch lines
-	love.graphics.line(-50, -60 * -9 + pitch_pixels, 50, -60 * -9 + pitch_pixels)
-	for i = -8,9 do
+	love.graphics.setFont(sans_label)
+	for i = -9,9 do
 		-- 10 degree line
 		love.graphics.line(-50, -60 * i + pitch_pixels, 50, -60 * i + pitch_pixels)
-		love.graphics.print(i * 10, 50, -60 * i + pitch_pixels)
+		if i ~= 0 then love.graphics.print(math.abs(i) * 10, 55, -60 * i + pitch_pixels - sans_label:getHeight() / 2) end
 
-		-- 5 degree line
-		love.graphics.line(-25, -60 * i + 30 + pitch_pixels, 25, -60 * i + 30 + pitch_pixels)
+		if i ~= -9 then
+			-- 5 degree line
+			love.graphics.line(-25, -60 * i + 30 + pitch_pixels, 25, -60 * i + 30 + pitch_pixels)
 
-		-- 2.5 degree lines
-		love.graphics.line(-10, -60 * i + 15 + pitch_pixels, 10, -60 * i + 15 + pitch_pixels)
-		love.graphics.line(-10, -60 * i + 45 + pitch_pixels, 10, -60 * i + 45 + pitch_pixels)
+			-- 2.5 degree lines
+			love.graphics.line(-10, -60 * i + 15 + pitch_pixels, 10, -60 * i + 15 + pitch_pixels)
+			love.graphics.line(-10, -60 * i + 45 + pitch_pixels, 10, -60 * i + 45 + pitch_pixels)
+		end
 	end
+	love.graphics.setFont(sans)
 
 	-- Resetting the reference
 	love.graphics.setScissor()
@@ -153,12 +161,13 @@ function love.draw()
 	-- Debug variable prints
 	love.graphics.origin()
 	love.graphics.setColor(1,1,1)
+	love.graphics.setFont(sans)
 	love.graphics.print("A " .. string.format("%.5f", data.altitude), 20, 20)
-	love.graphics.print("S " .. string.format("%.5f", data.ias), 20, 40)
-	love.graphics.print("V " .. string.format("%.5f", data.vspeed), 20, 60)
+	love.graphics.print("S " .. string.format("%.5f", data.ias), 20, 20 + sans:getHeight() + 5)
+	love.graphics.print("V " .. string.format("%.5f", data.vspeed), 20, 25 + 2 * sans:getHeight() + 5)
 	love.graphics.print("P " .. string.format("%.5f", data.pitch), 200, 20)
-	love.graphics.print("R " .. string.format("%.5f", data.roll), 200, 40)
-	love.graphics.print("H " .. string.format("%.5f", data.heading), 200, 60)
+	love.graphics.print("R " .. string.format("%.5f", data.roll), 200, 20 + sans:getHeight() + 5)
+	love.graphics.print("H " .. string.format("%.5f", data.heading), 200, 25 + 2 * sans:getHeight() + 5)
 	-- End debug variable prints
 end
 
